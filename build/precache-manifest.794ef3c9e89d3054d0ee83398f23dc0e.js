e5cf19feca30aefb2e19b3dc4f45fff4c20ff0db
luis <itt.luisperez@gmail.com>
2020-06-12 09:45:06 -0400

    first commit

---
+++ build/precache-manifest.794ef3c9e89d3054d0ee83398f23dc0e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6c96a121c640f2370eebafa9db130e2",
+    "url": "/robofriendstest/index.html"
+  },
+  {
+    "revision": "f00daa0a5187c663c77a",
+    "url": "/robofriendstest/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "f6b725f1a5ae79181bd0",
+    "url": "/robofriendstest/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f00daa0a5187c663c77a",
+    "url": "/robofriendstest/static/js/2.88e0f59c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriendstest/static/js/2.88e0f59c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6b725f1a5ae79181bd0",
+    "url": "/robofriendstest/static/js/main.b1aa57dc.chunk.js"
+  },
+  {
+    "revision": "664e0561f66a63a44fbb",
+    "url": "/robofriendstest/static/js/runtime-main.a7488d55.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/robofriendstest/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [ //not export default thats why when importing you need to do destructuring { }
+  {
+    id: 1,
+    name: 'Luis Perez',
+    username: 'Luisprz',
+    email: 'luisperez@klk.com'
+  },
+  {
+    id: 2,
+    name: 'isaazz',
+    username: 'isaazz',
+    email: 'isaazz@isaazz.isaazz'
+  },
+  {
+    id: 3,
+    name: 'El engel',
+    username: 'Engel',
+    email: 'Engel@yesenia.net'
+  },
+  {
+    id: 4,
+    name: 'Roman el celdo',
+    username: 'Roman',
+    email: 'Roman@klk.org'
+  },
+  {
+    id: 5,
+    name: 'Chelsey Dietrich',
+    username: 'Kamren',
+    email: 'Lucio_Hettinger@annie.ca'
+  },
+  {
+    id: 6,
+    name: 'Robenmy',
+    username: 'Robenmy',
+    email: 'klk@klk.info'
+  },
+  {
+    id: 7,
+    name: 'Kurtis Weissnat',
+    username: 'Elwyn.Skiles',
+    email: 'Telly.Hoeger@billy.biz'
+  },
+  {
+    id: 8,
+    name: 'Nicholas Runolfsdottir V',
+    username: 'Maxime_Nienow',
+    email: 'Sherwood@rosamond.me'
+  },
+  {
+    id: 9,
+    name: 'Glenna Reichert',
+    username: 'Delphine',
+    email: 'Chaim_McDermott@dana.io'
+  },
+  {
+    id: 10,
+    name: 'Clementina DuBuque',
+    username: 'Moriah.Stanton',
+    email: 'Rey.Padberg@karina.biz'
+  }
+];
