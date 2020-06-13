5c0cf5329be788e0f7107c068b1607f91e9d5824
luisprz <itt.luisperez@gmail.com>
2020-06-13 10:55:14 -0400

    Updates

---
+++ precache-manifest.de918c79ae87983887639bc4ea25bac9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10cfdcba7b0ad648488d764310f30251",
+    "url": "/robofriendsv2/index.html"
+  },
+  {
+    "revision": "7004e53df9d4e55d71ee",
+    "url": "/robofriendsv2/static/css/2.306a6dee.chunk.css"
+  },
+  {
+    "revision": "c7fa1e60793c194b7c35",
+    "url": "/robofriendsv2/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "7004e53df9d4e55d71ee",
+    "url": "/robofriendsv2/static/js/2.b81f41fd.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/robofriendsv2/static/js/2.b81f41fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7fa1e60793c194b7c35",
+    "url": "/robofriendsv2/static/js/main.bc1258f2.chunk.js"
+  },
+  {
+    "revision": "b81e0aebfdae9ea862a0",
+    "url": "/robofriendsv2/static/js/runtime-main.d7a6c478.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/robofriendsv2/static/media/logo.ee7cd8ed.svg"
+  }
+]);
